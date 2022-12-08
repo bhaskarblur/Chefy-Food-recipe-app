fb85c79c2fbeef9a31b079fa8bfa194e52001c51
2wizstudio <hello.2wizstudio@gmail.com>
2022-12-08 13:44:18 +0530

    #3

--- app/src/main/java/com/the2wizstudio/chefy/splashScreen.java
+++ app/src/main/java/com/the2wizstudio/chefy/splashScreen.java
@@ -2,7 +2,9 @@ package com.the2wizstudio.chefy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -15,5 +17,26 @@ public class splashScreen extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         this.getSupportActionBar().hide();
+
+        skipSplash();
+    }
+
+    private void skipSplash() {
+        // this will skip the splashScreen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splashScreen.this,onBoarding.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
     }
 }
+
