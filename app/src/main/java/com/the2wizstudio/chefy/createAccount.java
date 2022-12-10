021cc3f49b1bae6485c988507aba289e1374f98d
2wizstudio <hello.2wizstudio@gmail.com>
2022-12-10 12:15:07 +0530

    added createAccount

---
+++ app/src/main/java/com/the2wizstudio/chefy/createAccount.java
@@ -0,0 +1,14 @@
+package com.the2wizstudio.chefy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
